6c695fd40ef79c98b0f82bff5c4398e338131fd1
mintyleaf <mintyleafdev@gmail.com>
2025-05-08 21:23:43 +0400

    add opts to agent interface

--- pkg/agent/agent.go
+++ pkg/agent/agent.go
@@ -10,9 +10,11 @@ type Agent interface {
 	Run(
 		ctx context.Context,
 		state []llms.MessageContent,
+		opts ...llms.CallOption,
 	) (llms.MessageContent, error)
 	SimpleRun(
 		ctx context.Context,
 		input string,
+		opts ...llms.CallOption,
 	) (string, error)
 }
